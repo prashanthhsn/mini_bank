760a4d6043de85da7db69d4c45808e49e5c0a6a0
Prashanthprasad <prashanthhsn21@gmail.com>
2020-10-26 15:30:51 +0530

    correcting account number

--- bank/models.py
+++ bank/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class All_Customer(models.Model):
     Sl_No = models.AutoField(primary_key=True)
@@ -7,17 +8,20 @@ class All_Customer(models.Model):
     Name = models.CharField(max_length= 25)
     Account_No = models.CharField(editable=False,blank=False,max_length=13,null=True)
     Photo = models.ImageField(upload_to ='media' )
-    Aadhar_No = models.CharField(max_length=12)
+    Aadhar_No = models.CharField(max_length=14)
     Phone_No = models.CharField(max_length=10)
     
     def save(self,*args,**kwargs):
-        ph = str(self.Phone_No)
-        ph = ph[-4:]
-        self.Account_No = ph + str(self.Aadhar_No)
+        # ph = str(self.Phone_No)
+        # ph = ph[-4:]
+        d = self.Date.strftime("%Y%m%d")
+        self.Account_No = str(d) + str(self.Sl_No)
         print(self.Account_No)
         super(All_Customer, self).save(*args, **kwargs)
+    
     class Meta:
         unique_together = (("Sl_No","Account_No"),)
+    
     def __str__(self):
         return self.Name
     
@@ -29,3 +33,11 @@ class Chit_Batches(models.Model):
     End_Date = models.DateField()
     Number_of_Customers = models.IntegerField()
     Status = models.BooleanField()
+
+    def __str__(self):
+        return str(self.Batch_No)
+
+class Savings_Customer(models.Model):
+    Sl_No = models.AutoField(primary_key=True)
+    Date_Added = models.DateField()
+    Name = models.CharField(max_length= 25)
