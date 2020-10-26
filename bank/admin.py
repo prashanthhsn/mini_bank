ee71f5314fd66be7a12d386edbaea535173ecfad
Prashanthprasad <prashanthhsn21@gmail.com>
2020-10-26 10:50:02 +0530

    finished chit batches

--- bank/admin.py
+++ bank/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import All_Customer
+from .models import All_Customer,Chit_Batches
 # Register your models here.
 
 class All_CustomerAdmin(admin.ModelAdmin):
     list_display = ('Sl_No','Date','Name','Account_No','Photo','Aadhar_No','Phone_No',)
 admin.site.register(All_Customer,All_CustomerAdmin)
+
+class Chit_BatchesAdmin(admin.ModelAdmin):
+    list_display = ('Sl_No','Batch_No','Total_Fund','Start_Date','End_Date','Number_of_Customers','Status')
+admin.site.register(Chit_Batches,Chit_BatchesAdmin)
